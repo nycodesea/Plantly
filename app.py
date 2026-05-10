d6372d6b3879cd0a749e1499b1c351b4a586223b
nycodesea <nyanko.shootingstar@gmail.com>
2026-05-10 23:24:19 +0900

    add parameters and test

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 import requests
-from dash import Dash, html
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
 
 app = Dash(__name__)
 
-url = (
-    "https://api.open-meteo.com/v1/forecast"
-    "?latitude=35"
-    "&longitude=139"
-    "&current=temperature_2m,relative_humidity_2m"
-)
+url = "https://api.open-meteo.com/v1/forecast"
 
-data = requests.get(url).json()
-temp = data["current"]["temperature_2m"]
-humidity = data["current"]["relative_humidity_2m"]
+params = {
+    "latitude": 35,
+    "longitude": 139,
+    "hourly": "temperature_2m,relative_humidity_2m",
+}
 
-app.layout = (
-    html.Div(
-        [
-            html.H1("Plantly dots"),
-            html.P("Texter."),
-            html.P(f"Temperture : {temp}"),
-            html.P(f"Humidity : {humidity}"),
-        ]
-    ),
+data = requests.get(url, params=params).json()
+
+times = data["hourly"]["time"]
+temperatures = data["hourly"]["temperature_2m"]
+humidity = data["hourly"]["relative_humidity_2m"]
+
+df = pd.DataFrame({"time": times, "temperature": temperatures, "humidity": humidity})
+fig = px.line(
+    df,
+    x="time",
+    y=["temperature", "humidity"],
+    title="Temperature and Humidity Over Time",
 )
+app.layout = html.Div([html.H1("Plant Dashboard"), dcc.Graph(figure=fig)])
 
 
 app.run(debug=True)
